7896bc8fbc1bf49faab789494cf30b8739f9f8a6
emokia <lee_lionel_96@hotmail.com>
2024-06-09 02:50:28 +0800

    edit backend models to fit FE competency

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,86 +1,90 @@
-const User = require('../models/user.models')
-const bcrypt = require('bcrypt')
+const User = require("../models/user.models");
+const bcrypt = require("bcrypt");
 
-async function create (req, res) {
-    try {
-        const newUserDetails = req.body;
-        const newUser = await User.create(newUserDetails)
-        return res.status(201).json(newUser)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({error: error.message})
-    }
+async function create(req, res) {
+  try {
+    const newUserDetails = req.body;
+    const newUser = await User.create(newUserDetails);
+    return res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
 }
 
 async function signIn(req, res) {
-    try { 
-        const user = await User.findOne({ email: req.body.email})
-        console.log(user)
-        if(!user) throw new Error()
-        const match = await bcrypt.compare(req.body.password, user.password);
-        if (!match) throw new Error();
-        console.log('Sign in Successful')
-        res.status(201).send(user)
-    } catch {
-        res.status(400).json("Bad Credentials")
-    }
+  try {
+    const user = await User.findOne({
+      $or: [
+        {
+          email: req.body.input,
+        },
+        {
+          phoneNumber: req.body.input,
+        },
+      ],
+    });
+    console.log(user);
+    if (!user) throw new Error();
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) throw new Error();
+    console.log("Sign in Successful");
+    res.status(201).send(user, user.role);
+  } catch {
+    res.status(400).json("Bad Credentials");
+  }
 }
 
 async function updateProfile(req, res) {
-    try {
-        const {id} = req.params
-        const updatedProfile = await User.findByIdAndUpdate(
-            id.trim(),
-            req.body,
-            {new: true}
-        )
-        res.status(200).json(updatedProfile)
-    } catch (error) {
-        return res.status(500).json({error: error.message})
-    }
+  try {
+    const { id } = req.params;
+    const updatedProfile = await User.findByIdAndUpdate(id.trim(), req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedProfile);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 }
 
 async function getProfile(req, res) {
-    try {
-        const user = await User.find({_id: req.params})
-        return res.status(200).json(user)
-    } catch (error) {
-        return res.status(500).json({error: error.message})
-    }
+  try {
+    const user = await User.find({ _id: req.params });
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 }
 
 async function listTutors(req, res) {
-    try {
-        const tutors = await User.find({ role: 'tutor'})
-        res.status(200).json(tutors)
-    } catch(error) {
-        res.status(500).json({error : error.message})
-    }
+  try {
+    const tutors = await User.find({ role: "tutor" });
+    res.status(200).json(tutors);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 async function addFeedback(req, res) {
-    try {
-        const {id} = req.params
-        const feedback = req.body
+  try {
+    const { id } = req.params;
+    const feedback = req.body;
 
-        const user = await User.findById(id)
-        user.feedback.push(feedback)
-        await user.save()
+    const user = await User.findById(id);
+    user.feedback.push(feedback);
+    await user.save();
 
-        res.json({message : 'Feedback added successfully'})
-    } catch (error) {
-        res.status(500).json({error: 'Internal Server Error'})
-    }
+    res.json({ message: "Feedback added successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
-
-
-
 
 module.exports = {
-    create,
-    signIn,
-    updateProfile,
-    getProfile,
-    listTutors,
-    addFeedback
-}
+  create,
+  signIn,
+  updateProfile,
+  getProfile,
+  listTutors,
+  addFeedback,
+};
--- models/user.models.js
+++ models/user.models.js
@@ -1,30 +1,44 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
-const SALT_ROUNDS = 10
+const SALT_ROUNDS = 10;
 
-const levelSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
       type: String,
-      enum: [
-        "Pri 1",
-        "Pri 2",
-        "Pri 3",
-        "Pri 4",
-        "Pri 5",
-        "Pri 6",
-        "Sec 1",
-        "Sec 2",
-        "Sec 3",
-        "Sec 4",
-      ],
+      required: true,
     },
-  });
-  
-  const subjectSchema = new Schema({
-    name: {
+    email: {
       type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      minLength: 5,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      minLength: 8,
+      maxLength: 8,
+    },
+    role: {
+      type: String,
+      enum: ["tutor", "parent"],
+      required: true,
+    },
+    experience: {
+      type: Number,
+    },
+    subjects: {
+      type: [String],
       enum: [
         "English",
         "Mathematics",
@@ -38,81 +52,62 @@ const levelSchema = new Schema({
         "Physics",
         "History",
         "Geography",
-      ]
-    },
-    levels: [levelSchema],
-  });
-
-const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        required: true,
-    },
-    password: {
-        type: String, 
-        trim: true,
-        minLength: 5,
-        required: true,
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-        minLength: 8,
-        maxLength: 8,
-    },
-    role: {
-        type: String,
-        enum: ['tutor','parent'],
-        required: true
-    },
-    experience: {
-        type: Number,
-      },
-      subjects: [subjectSchema],
-      location: {
-        type: String,
-        enum: ["North", "North-East", "Central", "East", "West"],
-      },
-      feedback: [
-        {
-          sentBy: {
-            type: String
-          },
-          rating: {
-              type: Number,
-              min: 0,
-              max: 5,
-          },
-          text: {
-              type: String
-          },
-          timestamp: {
-              type: Date,
-              default: Date.now,
-          }
-        },
       ],
-}, {
+    },
+    level: {
+      type: [String],
+      enum: [
+        "Pri 1",
+        "Pri 2",
+        "Pri 3",
+        "Pri 4",
+        "Pri 5",
+        "Pri 6",
+        "Sec 1",
+        "Sec 2",
+        "Sec 3",
+        "Sec 4",
+      ],
+    },
+    location: {
+      type: String,
+      enum: ["North", "North-East", "Central", "East", "West"],
+    },
+    feedback: [
+      {
+        sentBy: {
+          type: String,
+        },
+        rating: {
+          type: Number,
+          min: 0,
+          max: 5,
+        },
+        text: {
+          type: String,
+        },
+        timestamp: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  {
     timestamps: true,
     toJSON: {
-      transform: function(doc, ret) {
-        delete ret.password
-        return ret
-      }
-    }
-})
+      transform: function (doc, ret) {
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
+);
 
-userSchema.pre('save', async function(next) {
-  if(!this.isModified('password')) return next()
-  this.password = await bcrypt.hash(this.password,SALT_ROUNDS)
-})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+});
 
-const User = mongoose.model("User", userSchema)
-module.exports = User
+const User = mongoose.model("User", userSchema);
+module.exports = User;
